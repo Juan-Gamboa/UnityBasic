7aa2f99821db55576601baa550f1356a6b793713
Juan-Gamboa <juaestnino@uniboyaca.edu.co>
2026-03-02 10:55:30 -0500

    Cambios Audio

--- Assets/Script/Activar sonido.cs
+++ Assets/Script/Activar sonido.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Activarsonido : MonoBehaviour
@@ -6,18 +8,19 @@ public class Activarsonido : MonoBehaviour
     public GameObject post1;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "PlayerInteractionZone")
         {
-            //AudioManager.Instance.Play3D("Entro", post.transform.position);
-            AudioManager.Instance.Play2D("Entro");
+            AudioManager.Instance.Play3D("Entro", post.transform.position);
+            //AudioManager.Instance.Play2D("Entro");
+            //Debug.Log("Mensaje");
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "PlayerInteractionZone")
         {
-            //AudioManager.Instance.Play3D("Salir", post1.transform.position);
-            AudioManager.Instance.Play2D("Salir");
+            AudioManager.Instance.Play3D("Salir", post1.transform.position);
+            //AudioManager.Instance.Play2D("Salir");
         }
     }
 }
